63abffffa52a29f80b4c78b723e205517adf5f2b
DecoMacie <119458392+DecoMacie@users.noreply.github.com>
2023-03-23 17:22:40 +0000

    Added Component files

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
 import React from "react";
 import "./App.css";
+import { Header } from "./components/Header";
 
 function App() {
-  return <div className="App">Bootcamp Project 2</div>;
+  return (
+    <div>
+      <header />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/Header.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export const Header = () => {
+  return (
+    <div>
+      <h1>Income vs Expense Planner</h1>
+    </div>
+  );
+};
