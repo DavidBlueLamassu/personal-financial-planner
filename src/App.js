62a698b63ad79518a3a83f1334b9b3d84bd48c22
DecoMacie <119458392+DecoMacie@users.noreply.github.com>
2023-03-26 02:07:36 +0100

    Added Home page button and Modal

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
 import React from "react";
 import "./App.css";
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import NavBar from './components/NavBar';
-import Home from './components/pages/Home';
-import PieChart from './components/pages/PieChart';
-import Forecast from './components/pages/Forecast';
-import Summary from './components/pages/Summary';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import NavBar from "./components/NavBar";
+import Home from "./components/pages/Home";
+import PieChart from "./components/pages/PieChart";
+import Forecast from "./components/pages/Forecast";
+import Summary from "./components/pages/Summary";
 
 function App() {
   return (
     <Router>
-      <div>
-        <NavBar />
-        {/* Wrap Route elements in a Routes component */}
+      <NavBar />
+      {/* Wrap Route elements in a Routes component */}
+      <div className="container">
         <Routes>
           {/* Define routes using the Route component to render different page components at different paths */}
           {/* Define a default route that will render the Home component */}
--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,21 +1,53 @@
-import acquireData from "./AcquireData.js"
+import * as React from "react";
+import Box from "@mui/material/Box";
+import Button from "@mui/material/Button";
+import Typography from "@mui/material/Typography";
+import Modal from "@mui/material/Modal";
+import acquireData from "./AcquireData";
 
-function Home() {
+const style = {
+  position: "absolute",
+  top: "50%",
+  left: "50%",
+  transform: "translate(-50%, -50%)",
+  width: 400,
+  bgcolor: "background.paper",
+  border: "2px solid #000",
+  boxShadow: 24,
+  p: 4,
+};
+
+export default function ModalWelcome() {
+  const [open, setOpen] = React.useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
+
   return (
     <div>
-      <h1>Welcome</h1>
-      <p>
-        In order to provide an analysis of your finances and to make a financial
-        forecast we will first need you to provide financial information. When
-        you are ready to begin, click START. Or if you would like to review an
-        earlier financial assessment click REVIEW. If you would like to delete
-        all financial data held in this system click CLEAR.
-      </p>
-      <button onClick={acquireData}>START</button>
-      <button>REVIEW</button>
-      <button>CLEAR</button>
+      <Button onClick={handleOpen}>Create/Edit profile</Button>
+      <Modal
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+      >
+        <Box sx={style}>
+          <Typography id="modal-modal-title" variant="h6" component="h2">
+            Welcome
+          </Typography>
+          <Typography id="modal-modal-description" sx={{ mt: 2 }}>
+            In order to provide an analysis of your finances and to make a
+            financial forecast we will first need you to provide financial
+            information. When you are ready to begin, click START. Or if you
+            would like to review an earlier financial assessment click REVIEW.
+            If you would like to delete all financial data held in this system
+            click CLEAR.
+          </Typography>
+          <Button onClick={acquireData}>START</Button>
+          <Button>REVIEW</Button>
+          <Button>CLEAR</Button>
+        </Box>
+      </Modal>
     </div>
   );
 }
-
-export default Home;
