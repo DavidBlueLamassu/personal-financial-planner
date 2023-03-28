6ced266c15d13be7e14679d4c85843aeb353883c
DecoMacie <119458392+DecoMacie@users.noreply.github.com>
2023-03-28 19:39:40 +0100

    Updated NAVBAR to make it responsive

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -8,76 +8,274 @@ import {
   Typography,
   Stack,
   Button,
-  // styled,
-  // Box,
+  Container,
+  Box,
+  MenuItem,
+  Menu,
 } from "@mui/material";
 import SavingsIcon from "@mui/icons-material/Savings";
-// import { TabList, TabPanel, TabContext } from "@mui/lab";
+import MenuIcon from "@mui/icons-material/Menu";
 
-// const useStyles = makeStyles({
-//   button: {
-//     "&.active": {
-//       background: "black",
-//     },
-//   },
-// });
+const pages = ["HOME", "CHARTS", "FORECAST", "SUMMARY"];
 
 function NavBar() {
-  // const [value, setValue] = React.useState("1");
-  // // still needs to change this...
-  // const handleChange = (event: React.SyntheticEvent, newValue: string) => {
-  //   setValue(newValue);
-  // };
+  const [anchorElNav, setAnchorElNav] = React.useState(null);
+
+  const handleOpenNavMenu = (event) => {
+    setAnchorElNav(event.currentTarget);
+  };
+  const handleCloseNavMenu = () => {
+    setAnchorElNav(null);
+  };
 
   return (
     <AppBar position="static">
-      <Toolbar>
-        <IconButton size="large" edge="start" color="inherit" aria-label="logo">
-          <SavingsIcon />
-        </IconButton>
-        <Typography
-          className="navTitle"
-          variant="h4"
-          component="div"
-          sx={{ flexGrow: 1 }}
-        >
-          PERSONAL FINANCIAL PLANNER
-        </Typography>
-        <Stack direction="row" spacing={2}>
-          <Button color="inherit" component={NavLink} to="/">
-            HOME
-          </Button>
-          <Button color="inherit" component={NavLink} to="/PieChart">
-            CHARTS
-          </Button>
-          <Button color="inherit" component={NavLink} to="/Forecast">
-            FORECAST
-          </Button>
-          <Button color="inherit" component={NavLink} to="/Summary">
-            SUMMARY
-          </Button>
-          <NavLink
-            to="//form"
-            className={({ isActive }) =>
-            isActive ? 'nav-link active' : 'nav-link'
-          }
+      <Container maxWidth="xl">
+        <Toolbar disableGutters>
+          <IconButton
+            size="large"
+            edge="start"
+            color="inherit"
+            aria-label="logo"
+            sx={{ display: { xs: "none", md: "flex" }, mr: 1 }}
           >
-            Form
-          </NavLink>
-        </Stack>
-      </Toolbar>
-    </AppBar>
+            <SavingsIcon />
+          </IconButton>
 
-    // <AppBar position="static">
-    //   <TabContext value={value}>
-    //     <TabList onChange={handleChange} aria-label="lab API tabs example">
-    //       <Tab label="Item One" value="1" />
-    //       <Tab label="Item Two" value="2" />
-    //       <Tab label="Item Three" value="3" />
-    //     </TabList>
-    //   </TabContext>
-    // </AppBar>
+          <Box sx={{ flexGrow: 1, display: { xs: "flex", sm: "none" } }}>
+            <IconButton
+              size="large"
+              aria-label="account of current user"
+              aria-controls="menu-appbar"
+              aria-haspopup="true"
+              onClick={handleOpenNavMenu}
+              color="inherit"
+            >
+              <MenuIcon />
+            </IconButton>
+            <Menu
+              id="menu-appbar"
+              anchorEl={anchorElNav}
+              anchorOrigin={{
+                vertical: "bottom",
+                horizontal: "left",
+              }}
+              keepMounted
+              transformOrigin={{
+                vertical: "top",
+                horizontal: "left",
+              }}
+              open={Boolean(anchorElNav)}
+              onClose={handleCloseNavMenu}
+              sx={{
+                display: { xs: "block", md: "none" },
+              }}
+            >
+              {pages.map((page) => (
+                <MenuItem key={page} onClick={handleCloseNavMenu}>
+                  <Typography textAlign="center">{page}</Typography>
+                </MenuItem>
+              ))}
+            </Menu>
+          </Box>
+
+          <Typography
+            className="navTitle"
+            variant="h4"
+            component="div"
+            sx={{ flexGrow: 1, display: { xs: "none", sm: "flex" } }}
+          >
+            PERSONAL FINANCIAL PLANNER
+          </Typography>
+          <Stack direction="row" spacing={2} sx={{ display: { sm: "flex" } }}>
+            <Button color="inherit" component={NavLink} to="/">
+              HOME
+            </Button>
+            <Button color="inherit" component={NavLink} to="/PieChart">
+              CHARTS
+            </Button>
+            <Button color="inherit" component={NavLink} to="/Forecast">
+              FORECAST
+            </Button>
+            <Button color="inherit" component={NavLink} to="/Summary">
+              SUMMARY
+            </Button>
+            <NavLink
+              to="//form"
+              className={({ isActive }) =>
+                isActive ? "nav-link active" : "nav-link"
+              }
+            >
+              {/* Form */}
+            </NavLink>
+          </Stack>
+        </Toolbar>
+      </Container>
+    </AppBar>
   );
 }
 
 export default NavBar;
+
+// import * as React from "react";
+// import AppBar from "@mui/material/AppBar";
+// import Box from "@mui/material/Box";
+// import Toolbar from "@mui/material/Toolbar";
+// import IconButton from "@mui/material/IconButton";
+// import Typography from "@mui/material/Typography";
+// import Menu from "@mui/material/Menu";
+// import MenuIcon from "@mui/icons-material/Menu";
+// import Container from "@mui/material/Container";
+// import Avatar from "@mui/material/Avatar";
+// import Button from "@mui/material/Button";
+// import Tooltip from "@mui/material/Tooltip";
+// import MenuItem from "@mui/material/MenuItem";
+// import AdbIcon from "@mui/icons-material/Adb";
+
+// const pages = ["Products", "Pricing", "Blog"];
+// const settings = ["Profile", "Account", "Dashboard", "Logout"];
+
+// function NavBar() {
+//   const [anchorElNav, setAnchorElNav] = React.useState(null);
+//   const [anchorElUser, setAnchorElUser] = React.useState(null);
+
+//   const handleOpenNavMenu = (event) => {
+//     setAnchorElNav(event.currentTarget);
+//   };
+//   const handleOpenUserMenu = (event) => {
+//     setAnchorElUser(event.currentTarget);
+//   };
+
+//   const handleCloseNavMenu = () => {
+//     setAnchorElNav(null);
+//   };
+
+//   const handleCloseUserMenu = () => {
+//     setAnchorElUser(null);
+//   };
+
+//   return (
+//     <AppBar position="static">
+//       <Container maxWidth="xl">
+//         <Toolbar disableGutters>
+//           <AdbIcon sx={{ display: { xs: "none", md: "flex" }, mr: 1 }} />
+//           <Typography
+//             variant="h6"
+//             noWrap
+//             component="a"
+//             href="/"
+//             sx={{
+//               mr: 2,
+//               display: { xs: "none", md: "flex" },
+//               fontFamily: "monospace",
+//               fontWeight: 700,
+//               letterSpacing: ".3rem",
+//               color: "inherit",
+//               textDecoration: "none",
+//             }}
+//           >
+//             LOGO
+//           </Typography>
+
+//           <Box sx={{ flexGrow: 1, display: { xs: "flex", md: "none" } }}>
+//             <IconButton
+//               size="large"
+//               aria-label="account of current user"
+//               aria-controls="menu-appbar"
+//               aria-haspopup="true"
+//               onClick={handleOpenNavMenu}
+//               color="inherit"
+//             >
+//               <MenuIcon />
+//             </IconButton>
+//             <Menu
+//               id="menu-appbar"
+//               anchorEl={anchorElNav}
+//               anchorOrigin={{
+//                 vertical: "bottom",
+//                 horizontal: "left",
+//               }}
+//               keepMounted
+//               transformOrigin={{
+//                 vertical: "top",
+//                 horizontal: "left",
+//               }}
+//               open={Boolean(anchorElNav)}
+//               onClose={handleCloseNavMenu}
+//               sx={{
+//                 display: { xs: "block", md: "none" },
+//               }}
+//             >
+//               {pages.map((page) => (
+//                 <MenuItem key={page} onClick={handleCloseNavMenu}>
+//                   <Typography textAlign="center">{page}</Typography>
+//                 </MenuItem>
+//               ))}
+//             </Menu>
+//           </Box>
+//           <AdbIcon sx={{ display: { xs: "flex", md: "none" }, mr: 1 }} />
+//           <Typography
+//             variant="h5"
+//             noWrap
+//             component="a"
+//             href=""
+//             sx={{
+//               mr: 2,
+//               display: { xs: "flex", md: "none" },
+//               flexGrow: 1,
+//               fontFamily: "monospace",
+//               fontWeight: 700,
+//               letterSpacing: ".3rem",
+//               color: "inherit",
+//               textDecoration: "none",
+//             }}
+//           >
+//             LOGO
+//           </Typography>
+//           <Box sx={{ flexGrow: 1, display: { xs: "none", md: "flex" } }}>
+//             {pages.map((page) => (
+//               <Button
+//                 key={page}
+//                 onClick={handleCloseNavMenu}
+//                 sx={{ my: 2, color: "white", display: "block" }}
+//               >
+//                 {page}
+//               </Button>
+//             ))}
+//           </Box>
+
+//           <Box sx={{ flexGrow: 0 }}>
+//             <Tooltip title="Open settings">
+//               <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
+//                 <Avatar alt="Remy Sharp" src="/static/images/avatar/2.jpg" />
+//               </IconButton>
+//             </Tooltip>
+//             <Menu
+//               sx={{ mt: "45px" }}
+//               id="menu-appbar"
+//               anchorEl={anchorElUser}
+//               anchorOrigin={{
+//                 vertical: "top",
+//                 horizontal: "right",
+//               }}
+//               keepMounted
+//               transformOrigin={{
+//                 vertical: "top",
+//                 horizontal: "right",
+//               }}
+//               open={Boolean(anchorElUser)}
+//               onClose={handleCloseUserMenu}
+//             >
+//               {settings.map((setting) => (
+//                 <MenuItem key={setting} onClick={handleCloseUserMenu}>
+//                   <Typography textAlign="center">{setting}</Typography>
+//                 </MenuItem>
+//               ))}
+//             </Menu>
+//           </Box>
+//         </Toolbar>
+//       </Container>
+//     </AppBar>
+//   );
+// }
+// export default NavBar;
